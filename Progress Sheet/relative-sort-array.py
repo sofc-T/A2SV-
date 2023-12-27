bfe56fd669fe2f598ca253479a888dc07ed5ba40
sofc-T <129575612+sofc-T@users.noreply.github.com>
2023-12-27 18:12:29 +0300

    Added solution for Relative Sort Array

--- Progress Sheet/relative-sort-array.py
+++ Progress Sheet/relative-sort-array.py
@@ -1,13 +1,6 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        freq = Counter(arr1)
-        res = []
-        uniques = []
-        set2 = set(arr2)
-        for num in arr1:
-            if num not in set2:
-                uniques.append(num)
-        uniques.sort()
-        for num in arr2:
-            res += [num] * freq[num]
-        return res + uniques
+        store = defaultdict(lambda: len(arr2))
+        for idx, num in enumerate(arr2):
+            store[num] = idx
+        return sorted(arr1,key=lambda x: (store[x],x))
