b36c3ea855467d7da0d2b0a8beac9be1e1f7aef1
sofc-T <129575612+sofc-T@users.noreply.github.com>
2023-12-29 17:18:06 +0300

    Added solution for Sorting the Sentence

--- Progress Sheet/sorting-the-sentence.py
+++ Progress Sheet/sorting-the-sentence.py
@@ -1,9 +1,5 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
-        nums = s.split(" ")
-        x = ''
-        for j in range(1,len(nums)+1):
-            for i in nums:
-                if int(i[len(i)-1]) == j:
-                    x = x + str(i.replace(i[len(i)-1] , " "))
-        return x[0:len(x)-1]
+        s = s.split()
+        s.sort(key=lambda x: int(x[-1]))
+        return " ".join([word[:-1] for word in s])
