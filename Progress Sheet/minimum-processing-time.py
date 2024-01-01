538ca2de3ef184b4244b268bd5b742ff13c28069
sofc-T <129575612+sofc-T@users.noreply.github.com>
2024-01-01 18:07:49 +0300

    Added solution for Minimum Processing Time

--- Progress Sheet/minimum-processing-time.py
+++ Progress Sheet/minimum-processing-time.py
@@ -1,11 +1,18 @@
 class Solution:
     def minProcessingTime(self, processorTime: List[int], tasks: List[int]) -> int:
-        tasks.sort()
-        processorTime.sort(reverse=True)
-        maxx, i = 0, 0
-        j = 0
-        for processor in processorTime:
-            maxx = max(maxx,max(tasks[i:i+(len(tasks)//len(processorTime))])+processorTime[j])
-            i += len(tasks)//len(processorTime)
-            j += 1
-        return maxx
+        # tasks.sort()
+        # processorTime.sort(reverse=True)
+        # maxx, i = 0, 0
+        # j = 0
+        # for processor in processorTime:
+        #     maxx = max(maxx,max(tasks[i:i+(len(tasks)//len(processorTime))])+processorTime[j])
+        #     i += len(tasks)//len(processorTime)
+        #     j += 1
+        # return maxx
+
+
+        
+        # s = zip(sorted(tasks),sorted(processorTime))
+        # t = zip(sorted(tasks)[::-4],sorted(processorTime))
+        # print(list(s),list(t))
+        return max(task + time for (task,time) in zip(sorted(tasks)[::-4],sorted(processorTime)))
