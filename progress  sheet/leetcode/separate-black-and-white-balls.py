f0ef64defe71bfa9b793fb83ae80954ce8211030
sofc-T <sofstzz@gmail.com>
2024-02-09 02:58:07 -0600

    Add solution for Separate Black and White Balls

--- progress  sheet/leetcode/separate-black-and-white-balls.py
+++ progress  sheet/leetcode/separate-black-and-white-balls.py
@@ -1,16 +1,23 @@
 class Solution:
     def minimumSteps(self, s: str) -> int:
-        l, r = 0 , 0
-        count, s = 0, list(s)
-        while l < len(s) and r  < len(s):
-            if s[l] == "0":
-                l += 1
-                r += 1
-            elif s[r] == "0":
-                count += r - l
-                s[l], s[r] = s[r], s[l]
-                l += 1
-                r += 1
+        # l, r = 0 , 0
+        # while l < len(s) and s[l] == "0":
+        #     l += 1
+        #     r += 1
+        # count = 0
+        # while l < len(s) and r  < len(s):
+        #     if s[r] == "0":
+        #         count += r - l
+        #         l += 1
+        #         r += 1
+        #     else:
+        #         r += 1
+        # return count
+
+        black, moves = 0 , 0
+        for val in s:
+            if val == "1":
+                black += 1
             else:
-                r += 1
-        return count
+                moves += black
+        return moves
