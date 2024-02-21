d723f2708b7a0477b1454c69ca992f371710f3e4
sofc-T <sofstzz@gmail.com>
2024-02-21 10:14:17 +0300

    Add solution for Reverse String

---
+++ progress  sheet/leetcode/reverse-string.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        l, r = 0 , len(s) - 1
+        def xo(l,r,s):
+            if l >= r: return s
+            s[l], s[r] = s[r], s[l]
+            return xo(l+1, r-1, s)
+        return xo(l,r,s)
